d3700d3e3057b134624f12b688f29f3f344c865a
Ec3o <2499302531@qq.com>
2024-10-10 21:29:25 +0800

    Updated

--- Front-End/h2o-frontend/src/App.jsx
+++ Front-End/h2o-frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Navbar from './components/NavBar'; // 确保路径正确
+import Navbar from './components/NavBar/NavBar'; // 确保路径正确
 import './index.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="p-4">
-        <h1 className="text-3xl">Welcome to the Blog!</h1>
-      </div>
     </div>
   );
 }
--- Front-End/h2o-frontend/src/components/NavBar/NavBar.jsx
+++ Front-End/h2o-frontend/src/components/NavBar/NavBar.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
-
+import './NavBar.css'
 function Navbar() {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Ec3o‘s Blog</a>
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
       </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-        </div>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full hover:scale-110">
-              <img
-                src="https://q.qlogo.cn/g?b=qq&nk=2499302531&s=100"
-                alt="User Avatar"
-              />
-            </div>
-          </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
           </ul>
-        </div>
-      </div>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
     </div>
+    <a className="btn btn-ghost text-xl">Ec3o's Blog</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>骇客杂记</a></li>
+      <li><a>生活碎笔</a></li>
+      <li><a>学海拾遗</a></li>
+      <li><a>小友之地</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">切换主题</a>
+  </div>
+</div>
   );
 }
 
